fc8ca3c9935c0904c028697aec8321cb415856f1
Mathis Da silva <mathis.dasilva23@gmail.com>
2026-02-09 16:33:53 +0000

    case rating

--- models/Intermediate/int_amazon_sales.sql
+++ models/Intermediate/int_amazon_sales.sql
@@ -1,1 +1,8 @@
-select * from {{ ref('stg_raw_amazon__sales') }}
+select * except(review_count),  
+CASE
+    WHEN rating < 3 THEN '🔴 Mauvais'
+    WHEN rating < 4  THEN '🟡 Moyen'
+    ELSE '🟢 Excellent'
+END as rating_cat,
+from {{ ref('stg_raw_amazon__sales') }}
+order by review_count asc 
