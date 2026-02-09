f5166af7a6110bebfc6631eb2673ebcfd7bf47db
Mathis Da silva <mathis.dasilva23@gmail.com>
2026-02-09 15:55:45 +0000

    stg faite

--- models/staging/raw_amazon/stg_raw_amazon__sales.sql
+++ models/staging/raw_amazon/stg_raw_amazon__sales.sql
@@ -1,2 +1,16 @@
-select * 
+select
+order_id,
+CAST(order_date AS DATE) as date_date,
+product_id,
+product_category,
+price,
+discount_percent,
+quantity_sold,
+customer_region,
+payment_method,
+rating,
+review_count,
+discounted_price,
+total_revenue,
 from {{ source('raw_amazon', 'sales') }}
+where total_revenue is not null
