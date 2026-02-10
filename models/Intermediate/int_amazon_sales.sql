1cb7fab3e37b4af3ba9b502b4c13b6ebd01d2aa8
Mathis Da silva <mathis.dasilva23@gmail.com>
2026-02-10 10:46:08 +0000

    Finition table Intermediaire, creation dossier Mart et début de table

--- models/Intermediate/int_amazon_sales.sql
+++ models/Intermediate/int_amazon_sales.sql
@@ -1,8 +1,25 @@
-select * except(review_count),  
+select
+order_id, 
+date_date, 
+product_id,
+product_category, 
+quantity_sold,
+price,
+discount_percent, 
+discounted_price,
 CASE
-    WHEN rating < 3 THEN '🔴 Mauvais'
-    WHEN rating < 4  THEN '🟡 Moyen'
+    WHEN discount_percent = 0 THEN 'No Discount'
+    WHEN discount_percent <= 10  THEN '1-10%'
+    WHEN discount_percent < 20  THEN '11-20%'
+    ELSE '21-30%'
+END as discount_bucket,
+total_revenue, 
+customer_region, 
+payment_method,
+rating,  
+CASE
+    WHEN rating < 3 THEN '🔴 Bad'
+    WHEN rating < 4  THEN '🟡 Average'
     ELSE '🟢 Excellent'
 END as rating_cat,
-from {{ ref('stg_raw_amazon__sales') }}
-order by review_count asc 
+from {{ ref('stg_raw_amazon__sales') }} 
